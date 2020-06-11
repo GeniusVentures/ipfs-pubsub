e937fcc1c650fb91e8cc9f1097306e0af37488ed
ruymaster <mecury1984@outlook.com>
2020-06-11 23:06:48 +0800

    update gitignore

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -5,6 +5,27 @@ function(disable_clang_tidy target)
       )
 endfunction()
 
+function(addtest test_name)
+  add_executable(${test_name} ${ARGN})
+  addtest_part(${test_name} ${ARGN})
+  target_link_libraries(${test_name}
+      GTest::gtest_main
+      GTest::gmock_main
+      )
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/xunit)
+  set(xml_output "--gtest_output=xml:${CMAKE_BINARY_DIR}/xunit/xunit-${test_name}.xml")
+  add_test(
+      NAME ${test_name}
+      COMMAND $<TARGET_FILE:${test_name}> ${xml_output}
+  )
+  set_target_properties(${test_name} PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/test_bin
+      ARCHIVE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
+      LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
+      )
+  disable_clang_tidy(${test_name})
+endfunction()
+
 add_custom_target(generated
     COMMENT "Building generated files..."
     )
