9bcda11e81ed34bce6bc54de6002203463bf45fd
creativeid00 <creativeid00@gmail.com>
2021-04-28 16:13:08 +0700

    GossipPubSub implementation added. #https://gitlab.geniusventures.io:8486/GeniusVentures/thirdparty/-/issues/6

---
+++ src/gossip_pubsub.hpp
@@ -0,0 +1,101 @@
+#ifndef GRPC_FOR_SUPERGENIUS_GOSSIBSUB
+#define GRPC_FOR_SUPERGENIUS_GOSSIBSUB
+
+#include <string>
+
+// boost/asio/io_context.hpp must be included before libp2p.
+// Otherwise an error of the second including of winsock.h is thrown on Windows
+#include <boost/asio/io_context.hpp>
+
+#include <libp2p/peer/peer_info.hpp>
+#include <libp2p/host/host.hpp>
+#include <libp2p/protocol/gossip/gossip.hpp>
+#include <libp2p/common/logger.hpp>
+#include <libp2p/outcome/outcome.hpp>
+
+/**
+* Codes for errors that originate in gossip pubsub
+*/
+enum class GossipPubSubError 
+{
+    INVALID_LOCAL_ADDRESS = 1,
+    FAILED_LOCAL_ADDRESS_LISTENING,
+    FAILED_SERVICE_START,
+};
+
+OUTCOME_HPP_DECLARE_ERROR_1(GossipPubSubError);
+
+/**
+* Gossip pubsub service.
+* Allows to subscribe to a named channel (topic) using linp2p Gossip pub-sub protocol
+* and publish short messages to the named channels
+*/
+class GossipPubSub final
+{
+public:
+    typedef libp2p::protocol::gossip::Gossip::Message Message;
+    typedef libp2p::protocol::Subscription Subscription;
+    typedef libp2p::protocol::gossip::Gossip::SubscriptionCallback MessageCallback;
+
+    /** Creates a gossip subscription service.
+    */
+    GossipPubSub(libp2p::common::Logger logger = libp2p::common::createLogger("GossipPubSub"));
+    ~GossipPubSub();
+
+    /** Subscribes the service to a specified topic.
+    * @param topic - a topic to subscribe to
+    * @param onMessageCallback - callback which is invoked when the topic messages are received
+    * @return subscription handler.
+    */ 
+    std::future<Subscription> Subscribe(const std::string& topic, MessageCallback onMessageCallback);
+
+    /** Publish a string message to specific topic.
+    * @param topic - a topic to publish a message to.
+    * @param message - published message
+    */
+    void Publish(const std::string& topic, const std::string& message);
+
+    /** Starts the gossip pubsub service.
+    * @param listeningPort - is a port for incoming connections.
+    * @param booststrapPeers - a list of remote peers that are connected to the current mesh
+    * @return future object that indicates if the service is started.
+    */
+    std::future<std::error_code>
+        Start(int listeningPort, const std::vector<std::string>& booststrapPeers);
+
+    /** Stops the gossip pubsub service.
+    */
+    void Stop();
+
+    /** Checks if the service is started.
+    * @return flag tha indicates if the service is started
+    */
+    bool IsStarted() const;
+
+    /** Returns the current service local address.
+    * @return a string representation of the current peer local multi-address
+    */
+    const std::string& GetLocalAddress();
+
+private:
+
+    std::shared_ptr<boost::asio::io_context> m_context;
+    std::unique_ptr<boost::asio::io_context::strand> m_strand;
+    std::shared_ptr<libp2p::Host> m_host;
+    std::shared_ptr<libp2p::protocol::gossip::Gossip> m_gossip;
+    std::unique_ptr<std::thread> _serviceThread;
+    std::string m_localAddress;
+    libp2p::common::Logger m_logger;
+};
+
+inline bool GossipPubSub::IsStarted() const
+{
+    return _serviceThread ? true : false;
+}
+
+inline const std::string& GossipPubSub::GetLocalAddress()
+{
+    return m_localAddress;
+}
+
+#endif // GRPC_FOR_SUPERGENIUS_GOSSIBSUB
---
+++ test/src/gossip_pubsub_test.cpp
@@ -0,0 +1,257 @@
+#include "gossip_pubsub.hpp"
+
+#include <gtest/gtest.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+/**
+ * @given A pubsub service shich is subscribed to a single topic
+ * @when A message is published to a topic that the service is subscribed to.
+ * @then The messages is received by the service.
+ */
+TEST(GossipPubSubTest, SendMessageToSingleSubscribedTopic)
+{
+    std::vector<std::string> receivedMessages;
+    GossipPubSub pubs;
+    pubs.Start(40001, {});
+    auto pubsTopic1 = pubs.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+    {
+        if (message)
+        {
+            std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+            receivedMessages.push_back(std::move(message));
+        }
+    });
+
+    std::string message("topic1_message");
+    pubs.Publish("topic1", message);
+
+    pubs.Stop();
+
+    ASSERT_EQ(receivedMessages.size(), 1);
+    EXPECT_EQ(receivedMessages[0], message);
+}
+
+/**
+ * @given A pubsub service shich is subscribed to a single topic
+ * @when A message is published to a topic that the service is not subscribed to.
+ * @then No messages received.
+ */
+TEST(GossipPubSubTest, SendMessageToUnsubscribedTopic)
+{
+    std::vector<std::string> receivedMessages;
+    GossipPubSub pubs;
+    pubs.Start(40001, {});
+    auto pubsTopic1 = pubs.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessages.push_back(std::move(message));
+            }
+        });
+
+    std::string message("topic2_message");
+    pubs.Publish("topic2", message);
+
+    ASSERT_EQ(receivedMessages.size(), 0);
+}
+
+/**
+ * @given A pubsub service which is subscribed to 2 different topics 
+ * @when A message is published to a specific topic.
+ * @then The message processor linked to the topic is executed to process the received message
+ */
+TEST(GossipPubSubTest, MessagesMutiplexing)
+{
+    std::vector<std::string> receivedMessagesTopic1;
+    std::vector<std::string> receivedMessagesTopic2;
+    GossipPubSub pubs;
+    pubs.Start(40001, {});
+    auto pubsTopic1 = pubs.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessagesTopic1.push_back(std::move(message));
+            }
+        });
+
+    auto pubsTopic2 = pubs.Subscribe("topic2", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessagesTopic2.push_back(std::move(message));
+            }
+        });
+
+    std::string messageTopic1("topic1_message");
+    pubs.Publish("topic1", messageTopic1);
+
+    std::string messageTopic2("topic2_message");
+    pubs.Publish("topic2", messageTopic2);
+
+    pubs.Stop();
+
+    ASSERT_EQ(receivedMessagesTopic1.size(), 1);
+    EXPECT_EQ(receivedMessagesTopic1[0], messageTopic1);
+
+    ASSERT_EQ(receivedMessagesTopic2.size(), 1);
+    EXPECT_EQ(receivedMessagesTopic2[0], messageTopic2);
+}
+
+/**
+ * @given 2 pubsub services that are subscribed to 2 different topics
+ * @when A message is published to a specific topic.
+ * @then The message processor linked to the topic is executed to process the received message
+ */
+TEST(GossipPubSubTest, MutipleGossipSubObjectsOnDifferentChannels)
+{
+    std::vector<std::string> receivedMessagesTopic1;
+    std::vector<std::string> receivedMessagesTopic2;
+    GossipPubSub pubs1;
+    pubs1.Start(40001, {});
+    GossipPubSub pubs2;
+    pubs2.Start(40002, {});
+    auto pubsTopic1 = pubs1.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessagesTopic1.push_back(std::move(message));
+            }
+        });
+
+    auto pubsTopic2 = pubs2.Subscribe("topic2", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessagesTopic2.push_back(std::move(message));
+            }
+        });
+
+    std::string messageTopic1("topic1_message");
+    pubs1.Publish("topic1", messageTopic1);
+
+    std::string messageTopic2("topic2_message");
+    pubs2.Publish("topic2", messageTopic2);
+
+    pubs1.Stop();
+    pubs2.Stop();
+
+    ASSERT_EQ(receivedMessagesTopic1.size(), 1);
+    EXPECT_EQ(receivedMessagesTopic1[0], messageTopic1);
+
+    ASSERT_EQ(receivedMessagesTopic2.size(), 1);
+    EXPECT_EQ(receivedMessagesTopic2[0], messageTopic2);
+}
+
+/**
+ * @given 2 pubsub services that are subscribed to a single topic
+ * @when A message is published to a specific topic.
+ * @then 2 message processor linked to the topic are xecuted to process the received message
+ */
+TEST(GossipPubSubTest, MutipleGossipSubObjectsOnSingleChannel)
+{
+    std::vector<std::string> receivedMessagesPubs1Topic1;
+    std::vector<std::string> receivedMessagesPubs2Topic1;
+    GossipPubSub pubs1;
+    pubs1.Start(40001, {});
+    GossipPubSub pubs2;
+    pubs2.Start(40001, { pubs1.GetLocalAddress() } );
+
+    auto pubs1Topic1 = pubs1.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessagesPubs1Topic1.push_back(std::move(message));
+            }
+        });
+
+    auto pubs2Topic1 = pubs2.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessagesPubs2Topic1.push_back(std::move(message));
+            }
+        });
+
+    // Wait for subscriptions
+    pubs1Topic1.wait();
+    pubs2Topic1.wait();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::string messageTopic1("topic1_message");
+    pubs2.Publish("topic1", messageTopic1);
+
+    // Wait for message transmitting
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    pubs2.Stop();
+    pubs1.Stop();
+
+    ASSERT_EQ(receivedMessagesPubs1Topic1.size(), 1);
+    EXPECT_EQ(receivedMessagesPubs1Topic1[0], messageTopic1);
+
+    ASSERT_EQ(receivedMessagesPubs2Topic1.size(), 1);
+    EXPECT_EQ(receivedMessagesPubs2Topic1[0], messageTopic1);
+}
+
+/**
+ * @given A pubsub service which is subscribed to a single topic
+ * @when Cancel a subscription before a maesage publishing
+ * @then No messages should be received
+ */
+TEST(GossipPubSubTest, CancelSubscription)
+{
+    std::vector<std::string> receivedMessages;
+    auto logger = spdlog::basic_logger_mt("GossipPubSubFile", "GossipPubSub.log");
+    logger->set_pattern("[%Y-%m-%d %H:%M:%S][%l] %v");
+    logger->set_level(spdlog::level::debug);
+
+    GossipPubSub pubs(logger);
+    pubs.Start(40001, {});
+    auto pubsTopic1 = pubs.Subscribe("topic1", [&](boost::optional<const GossipPubSub::Message&> message)
+        {
+            if (message)
+            {
+                std::string message(reinterpret_cast<const char*>(message->data.data()), message->data.size());
+                receivedMessages.push_back(std::move(message));
+            }
+        });
+
+    // Cancel sunscription before message publishing
+    pubsTopic1.get().cancel();
+
+    std::string message("topic1_message");
+    pubs.Publish("topic1", message);
+
+    pubs.Stop();
+
+    ASSERT_EQ(receivedMessages.size(), 0);
+}
+
+/**
+ * @given A pubsub service
+ * @when The service is started twice
+ * @then The second start failed
+ */
+TEST(GossipPubSubTest, DISABLED_SecondStartFailed)
+{
+    std::vector<std::string> receivedMessages;
+    auto logger = spdlog::basic_logger_mt("GossipPubSub", "GossipPubSub.log");
+    logger->set_pattern("[%Y-%m-%d %H:%M:%S][%l] %v");
+    logger->set_level(spdlog::level::debug);
+
+    GossipPubSub pubs(logger);
+    pubs.Start(40001, {});
+    auto futureResult = pubs.Start(40001, {});
+
+    auto result = futureResult.get();
+    ASSERT_EQ(bool(result), true);
+    EXPECT_EQ(result.message(), "");
+}
