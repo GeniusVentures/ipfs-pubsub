f73d1899b284393fac5f1c6d916b619024f90f79
sariru@gmail.com <89619301+itsafuu@users.noreply.github.com>
2024-10-09 15:19:04 -0400

    Reduce load temporarily for testing

--- src/ipfs_pubsub/gossip_pubsub.cpp
+++ src/ipfs_pubsub/gossip_pubsub.cpp
@@ -447,6 +447,7 @@ std::future<std::error_code> GossipPubSub::Start(
     }
 
     void GossipPubSub::ScheduleNextFind(const libp2p::multi::ContentIdentifier& cid, std::chrono::seconds interval) {
+        return;
         if (!m_timer) {
             m_timer = std::make_shared<boost::asio::steady_timer>(*m_context);
         }
@@ -461,6 +462,7 @@ std::future<std::error_code> GossipPubSub::Start(
     }
 
     void GossipPubSub::ScheduleNextFind(const libp2p::protocol::kademlia::ContentId& cid, std::chrono::seconds interval) {
+        return;
         if (!m_timer) {
             m_timer = std::make_shared<boost::asio::steady_timer>(*m_context);
         }
